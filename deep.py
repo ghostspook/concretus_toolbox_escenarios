6d2758c2e4b2081069cc73ba31f4ddbf9382626a
Rafael Castillo <pcastill@mac.com>
2022-08-02 16:42:21 -0500

    Cargar escenario y computar acumulado

--- deep.py
+++ deep.py
@@ -5,14 +5,39 @@ def abrir_plan_cuentas():
     return df
 
 def nuevo_ambiente():
+    print ('Creando ambiente ...')
+
     df = abrir_plan_cuentas()
     df['cu_codigo'] = df['cu_codigo'].astype(str)
     FORMAT = ['cu_codigo', 'cu_nombre', 'cu_nivel', 'cu_cuenta_padre']
     result = df[FORMAT].set_index('cu_codigo')
     result['cu_cuenta_padre'] = result['cu_cuenta_padre'].astype(str)
+    imprimir_columnas(result)
     return result
 
-def cargar_saldos_sabana(ambiente: pandas.DataFrame, filename: str):
+def cargar_escenario(ambiente: pandas.DataFrame, filename: str):
+    print ('Cargando escenario ...')
+
+    df = pandas.read_excel(filename)
+    columna_codigo_cuenta = df.keys()[0]
+    df[columna_codigo_cuenta] = df[columna_codigo_cuenta].astype(str)
+    df = df.set_index(columna_codigo_cuenta)
+    resultado = ambiente.copy()
+    column_names = df.keys()
+    new_column_name = column_names[len(column_names) - 1]
+    resultado[new_column_name] = 0
+    for index_codigo_cuenta, row in df.iterrows():
+        monto = row[new_column_name]
+        if (index_codigo_cuenta in resultado.index):
+            resultado.at[index_codigo_cuenta, new_column_name] = monto
+        else:
+            print('Código no consta en el plan de cuentas', index_codigo_cuenta)
+    imprimir_columnas(resultado)
+    return resultado
+
+def cargar_netos_movimientos(ambiente: pandas.DataFrame, filename: str):
+    print ('Cargando montos netos de movimientos ...')
+
     df = pandas.read_excel(filename)
     columna_codigo_cuenta = df.keys()[0]
     df[columna_codigo_cuenta] = df[columna_codigo_cuenta].astype(str)
@@ -26,7 +51,8 @@ def cargar_saldos_sabana(ambiente: pandas.DataFrame, filename: str):
         if (index_codigo_cuenta in resultado.index):
             actualizar_monto_recursivamente(resultado, index_codigo_cuenta, new_column_name, saldo)
         else:
-            print ('Código no consta en el plan de cuentas', index_codigo_cuenta)
+            print ('Código no consta en el plan de cuentas', index_codigo_cuenta, '. Monto: ', saldo)
+    imprimir_columnas(resultado)
     return resultado
 
 def actualizar_monto_recursivamente(resultado, codigo_cuenta, columna_montos, nuevo_saldo):
@@ -35,3 +61,20 @@ def actualizar_monto_recursivamente(resultado, codigo_cuenta, columna_montos, nu
     codigo_cuenta_padre = resultado.at[codigo_cuenta, 'cu_cuenta_padre']
     if ((codigo_cuenta_padre != '0') & (codigo_cuenta_padre != '')):
         actualizar_monto_recursivamente(resultado, codigo_cuenta_padre, columna_montos, nuevo_saldo)
+
+def computar_acumulado(ambiente: pandas.DataFrame, columna_escenario_inicial: str, columna_neto_movimientos: str, columna_resultado: str):
+    print ('Calculando resultado acumulado ...')
+
+    resultado = ambiente.copy()
+    resultado[columna_resultado] = 0
+    for index_codigo_cuenta, row in resultado.iterrows():
+        valor_escenario_inicial = row[columna_escenario_inicial]
+        neto_movimientos = row[columna_neto_movimientos]
+        resultado.at[index_codigo_cuenta, columna_resultado] = valor_escenario_inicial + neto_movimientos
+    imprimir_columnas(resultado)
+    return resultado
+def imprimir_columnas(df: pandas.DataFrame):
+    print("Columnas:")
+    for col in df.columns:
+        print('- ', col)
+    print("\n")
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import deep
 
 ambiente = deep.nuevo_ambiente()
 
-ambiente = deep.cargar_saldos_sabana(ambiente, 'input/SAENZ RONQUILLO_DICIEMBRE_2021.xlsx')
+ambiente = deep.cargar_escenario(ambiente, 'input/SAENZ RONQUILLO_NOVIEMBRE_2021.xlsx')
+ambiente = deep.cargar_netos_movimientos(ambiente, 'input/SAENZ RONQUILLO_DICIEMBRE_2021_(netos_movimientos).xlsx')
+ambiente = deep.computar_acumulado(ambiente, 'SAENZ RONQUILLO_NOVIEMBRE_2021', 'SAENZ RONQUILLO_DICIEMBRE_2021 (netos movimientos)', 'SAENZ RONQUILLO_DICIEMBRE_2021')
+
 ambiente.to_excel("output/ambiente.xlsx")
 # print(ambiente['SAENZ RONQUILLO_DICIEMBRE_2021'])
